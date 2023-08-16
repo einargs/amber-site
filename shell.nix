72ca26bc256b4ec2a63cac5e39f5a272b41a08d0
Einar Strandberg <einargs@gmail.com>
2023-08-15 19:48:50 -0500

    Got jekyll installed and running with default minima theme

--- shell.nix
+++ shell.nix
@@ -1,4 +1,27 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ bundler ];
+
+with pkgs;
+
+# Using: https://matthewrhone.dev/jekyll-in-nixos
+# Could not get it to properly work because of problems with
+# platform specific builds messing with hashes. I figured out
+# a way to tell bundler not to do that with
+# `bundler config set --local force_ruby_platform false`.
+# However, then I got a build error from sass complaining about not
+# being able to build the platform specific version (which it needed
+# TCP access for???). So we're just using normal bundler.
+
+mkShell {
+  nativeBuildInputs = [bundler ruby];
 }
+
+/*let env = bundlerEnv {
+  name = "amber-site";
+  inherit ruby;
+  gemfile = ./Gemfile;
+  lockfile = ./Gemfile.lock;
+  gemset = ./gemset.nix;
+}; in stdenv.mkDerivation {
+  name = "amber-site";
+  buildInputs = [env bundler ruby];
+}*/
